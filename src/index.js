d8d26f5beed4448d3eeaa4d298b882bb64d9bf43
Garin87 <kamrader@gmail.com>
2018-09-18 22:54:27 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+* @param preferences - an array of integers. Indices of people, whom they love
+* @returns number of love triangles
+*/
+module.exports = function getLoveTrianglesCount(arr = []) {
+
+var count= 0;
+var flag = false; 
+for( var i = 0; i < arr.length; i++){
+  var SpichoneeA = arr[i] - 1 ;
+  var SpichoneeB = arr[SpichoneeA] - 1;
+  var SpichoneeC = arr[SpichoneeB] - 1;
+
+  if(SpichoneeA == i){ flag = true};
+  if (SpichoneeA == SpichoneeB) { flag =  true};
+  if (SpichoneeB == SpichoneeC){ flag = true};
+  if (i == SpichoneeC) { count++};
+      
+}
+
+return Math.floor(count/3);
+
 };
